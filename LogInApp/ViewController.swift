253eff8e0643db4588e1161eb3432a36f96e2f86
Ovechkina Diana <ovechkinadiana@yandex.ru>
2021-12-14 20:28:17 +0300

    Initial Commit

---
+++ LogInApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogInApp
+//
+//  Created by Диана Овечкина on 14.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
